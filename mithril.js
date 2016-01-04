2c442b358b85d4353a6e81016b5244f623d75c30
Vincent Velociter <vincent.velociter@gmail.com>
2016-01-04 21:26:11 +0100

    Remove v8 deopts

--- mithril.js
+++ mithril.js
@@ -101,19 +101,13 @@ var m = (function app(window, undefined) {
 	// This function was causing deopts in Chrome.
 	function dataToString(data) {
 		//data.toString() might throw or return null if data is the return value of Console.log in Firefox (behavior depends on version)
-		try {
-			if (data == null || data.toString() == null) return "";
-		} catch (e) {
-			return "";
-		}
+    if (data == null || data.toString() == null) return "";
 		return data;
 	}
 	// This function was causing deopts in Chrome.
 	function injectTextNode(parentElement, first, index, data) {
-		try {
-			insertNode(parentElement, first, index);
-			first.nodeValue = data;
-		} catch (e) {} //IE erroneously throws error when appending an empty text node after a null
+    insertNode(parentElement, first, index);
+    first.nodeValue = data;
 	}
 
 	function flatten(list) {
@@ -553,40 +547,34 @@ var m = (function app(window, undefined) {
 			var cachedAttr = cachedAttrs[attrName];
 			if (!(attrName in cachedAttrs) || (cachedAttr !== dataAttr)) {
 				cachedAttrs[attrName] = dataAttr;
-				try {
-					//`config` isn't a real attributes, so ignore it
-					if (attrName === "config" || attrName === "key") continue;
-					//hook event handlers to the auto-redrawing system
-					else if (isFunction(dataAttr) && attrName.slice(0, 2) === "on") {
-						node[attrName] = autoredraw(dataAttr, node);
-					}
-					//handle `style: {...}`
-					else if (attrName === "style" && dataAttr != null && isObject(dataAttr)) {
-						for (var rule in dataAttr) {
-							if (cachedAttr == null || cachedAttr[rule] !== dataAttr[rule]) node.style[rule] = dataAttr[rule];
-						}
-						for (var rule in cachedAttr) {
-							if (!(rule in dataAttr)) node.style[rule] = "";
-						}
-					}
-					//handle SVG
-					else if (namespace != null) {
-						if (attrName === "href") node.setAttributeNS("http://www.w3.org/1999/xlink", "href", dataAttr);
-						else node.setAttribute(attrName === "className" ? "class" : attrName, dataAttr);
-					}
-					//handle cases that are properties (but ignore cases where we should use setAttribute instead)
-					//- list and form are typically used as strings, but are DOM element references in js
-					//- when using CSS selectors (e.g. `m("[style='']")`), style is used as a string, but it's an object in js
-					else if (attrName in node && attrName !== "list" && attrName !== "style" && attrName !== "form" && attrName !== "type" && attrName !== "width" && attrName !== "height") {
-						//#348 don't set the value if not needed otherwise cursor placement breaks in Chrome
-						if (tag !== "input" || node[attrName] !== dataAttr) node[attrName] = dataAttr;
-					}
-					else node.setAttribute(attrName, dataAttr);
-				}
-				catch (e) {
-					//swallow IE's invalid argument errors to mimic HTML's fallback-to-doing-nothing-on-invalid-attributes behavior
-					if (e.message.indexOf("Invalid argument") < 0) throw e;
-				}
+        //`config` isn't a real attributes, so ignore it
+        if (attrName === "config" || attrName === "key") continue;
+        //hook event handlers to the auto-redrawing system
+        else if (isFunction(dataAttr) && attrName.slice(0, 2) === "on") {
+          node[attrName] = autoredraw(dataAttr, node);
+        }
+        //handle `style: {...}`
+        else if (attrName === "style" && dataAttr != null && isObject(dataAttr)) {
+          for (var rule in dataAttr) {
+            if (cachedAttr == null || cachedAttr[rule] !== dataAttr[rule]) node.style[rule] = dataAttr[rule];
+          }
+          for (var rule in cachedAttr) {
+            if (!(rule in dataAttr)) node.style[rule] = "";
+          }
+        }
+        //handle SVG
+        else if (namespace != null) {
+          if (attrName === "href") node.setAttributeNS("http://www.w3.org/1999/xlink", "href", dataAttr);
+          else node.setAttribute(attrName === "className" ? "class" : attrName, dataAttr);
+        }
+        //handle cases that are properties (but ignore cases where we should use setAttribute instead)
+        //- list and form are typically used as strings, but are DOM element references in js
+        //- when using CSS selectors (e.g. `m("[style='']")`), style is used as a string, but it's an object in js
+        else if (attrName in node && attrName !== "list" && attrName !== "style" && attrName !== "form" && attrName !== "type" && attrName !== "width" && attrName !== "height") {
+          //#348 don't set the value if not needed otherwise cursor placement breaks in Chrome
+          if (tag !== "input" || node[attrName] !== dataAttr) node[attrName] = dataAttr;
+        }
+        else node.setAttribute(attrName, dataAttr);
 			}
 			//#348 dataAttr may not be a string, so use loose comparison (double equal) instead of strict (triple equal)
 			else if (attrName === "value" && tag === "input" && node.value != dataAttr) {
